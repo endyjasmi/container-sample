1f6e7709d6026a2b81528dbd67fccf3b652468a8
Endy Jasmi <endyjasmi@gmail.com>
2021-12-06 16:24:32 +0800

    feat: add container error implementation

--- source/errors/index.ts
+++ source/errors/index.ts
@@ -1,5 +1,6 @@
 export * from "./base-error.js";
 export * from "./binder-error.js";
 export * from "./binding-error.js";
+export * from "./container-error.js";
 export * from "./resolver-error.js";
 export * from "./scope-error.js";
